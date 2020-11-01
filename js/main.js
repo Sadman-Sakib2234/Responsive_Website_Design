fb0dcbaea2bd9ce186540ad41bd8c860d188ad14
Sadman_Sakib <61703195+Sadman-Sakib2234@users.noreply.github.com>
2020-11-01 14:35:06 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  var menuToggle = document.querySelector(".menutoggle");
+  var naviToggle = document.querySelector(".navication");
+  menuToggle.classList.toggle("active");
+  naviToggle.classList.toggle("active");
+}
